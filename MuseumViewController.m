b7df6472908e88d9c00340cb618dd39976ea2fac
Jon Fox <foxjon123@gmail.com>
2015-03-07 20:33:37 -0600

    Adds animations

--- MuseumViewController.m
+++ MuseumViewController.m
@@ -9,10 +9,28 @@
 #import "MuseumViewController.h"
 
 @interface MuseumViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *blueContainerConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *redContainerConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *yellowContainerConstraint;
 @property (nonatomic) NSString *chosenTourColor;
 @end
 
 @implementation MuseumViewController
+
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    self.blueContainerConstraint.constant = 600;
+    self.redContainerConstraint.constant = 600;
+    self.yellowContainerConstraint.constant = 600;
+    [self.view layoutIfNeeded];
+
+}
+
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:YES];
+    [self animateTourLabels];
+}
 
 
 - (IBAction)tourButtonPressed:(UIButton *)sender {
@@ -20,4 +38,13 @@
     NSLog(@"%@", sender.titleLabel.text);
 }
 
+
+-(void)animateTourLabels{
+    [UIView animateWithDuration:1.0 animations:^{
+        self.blueContainerConstraint.constant = 282;
+        self.redContainerConstraint.constant = 348;
+        self.yellowContainerConstraint.constant = 418;
+        [self.view layoutIfNeeded];
+    }];
+}
 @end
